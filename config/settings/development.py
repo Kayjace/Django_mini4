78a5c7d3106b778d4a8ad7c916ac16bfc8b2a850
Kayjace <gapr0317@gmail.com>
2024-10-25 12:23:27 +0900

    ✨ feat: GitHub Actions CI 설정 완료 및 시크릿 사용 설정

--- config/settings/development.py
+++ config/settings/development.py
@@ -1,14 +1,15 @@
 from .base import *
+import os
 
 DEBUG = True
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'mini4',
-        'USER': 'admin',
-        'PASSWORD': '0000',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': os.environ.get('DB_HOST'),
+        'PORT': os.environ.get('DB_PORT'),
     }
 }
