321a5e96d40d98ec5c8bf8e82bad82bcc3a0c52c
Kayjace <gapr0317@gmail.com>
2024-10-25 12:03:10 +0900

    ✨ feat: GitHub Actions CI 설정 추가

--- config/settings/development.py
+++ config/settings/development.py
@@ -5,9 +5,9 @@ DEBUG = True
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'your_dev_db_name',
-        'USER': 'your_dev_user',
-        'PASSWORD': 'your_dev_password',
+        'NAME': 'mini4',
+        'USER': 'admin',
+        'PASSWORD': '0000',
         'HOST': 'localhost',
         'PORT': '5432',
     }
