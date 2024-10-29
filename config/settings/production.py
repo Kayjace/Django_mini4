f17f94420c116c9912205c8d21dbd71c729866df
Kayjace <gapr0317@gmail.com>
2024-10-29 15:39:32 +0900

    🚚 build: Production
    프로덕션용 코드

--- config/settings/production.py
+++ config/settings/production.py
@@ -1,4 +1,28 @@
+from dotenv import load_dotenv
+
 from .base import *
 
 DEBUG = False
-# 배포 환경 특정 설정
+
+load_dotenv()  # .env 파일 로드
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("DB_NAME"),
+        "USER": os.environ.get("DB_USER"),
+        "PASSWORD": os.environ.get("DB_PASSWORD"),
+        "HOST": os.environ.get("DB_HOST"),
+        "PORT": os.environ.get("DB_PORT"),
+    }
+}
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+
+# 기본 스케줄 설정
+CELERY_BEAT_SCHEDULE = {}
